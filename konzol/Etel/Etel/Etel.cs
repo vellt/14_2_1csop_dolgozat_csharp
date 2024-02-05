0e9e9272754a843d06ee3ceb9ac70b0230735c60
diak <diak@mesterlevel.lo>
2024-02-05 10:10:06 +0100

    konzol potlas

---
+++ konzol/Etel/Etel/Etel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Etel
+{
+    public class Etel
+    {
+        public int id { get; set; }
+        public string neve { get; set; }
+        public int energia { get; set; }
+        public int szenh { get; set; }
+        public int ara { get; set; }
+        public string kategoria { get; set; }
+
+    }
+}
